c2f4307f1e0f5cd5e966db781f139773f7655b7c
Apoorva srivastva <140163599+appusrivastva@users.noreply.github.com>
2025-06-02 18:01:05 +0530

    Interaction with Smart Contract.....

---
+++ interactionSC.js
@@ -0,0 +1,183 @@
+const ethers = require("ethers");
+
+const provider = new ethers.JsonRpcProvider(
+  "https://data-seed-prebsc-2-s3.binance.org:8545/"
+);
+
+const address = "0x4352cB7EcdeF65a8b50d22f506bE9cD5f8fC5D19";
+
+const abi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "_rTotal",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "_tTotal",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "taxFee",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "rAmount",
+        type: "uint256",
+      },
+    ],
+    name: "tokenFromReflection",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "recipient",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "transfer",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+//readoperation 
+const interactionSc=async()=>{
+    const contract=new ethers.Contract(address,abi,provider);
+    console.log(contract)
+
+    const tokenName=await contract.name()
+    console.log(tokenName)
+
+    const symbol=await contract.symbol()
+    console.log(symbol)
+
+    const balance=await contract.balanceOf("0x8452203ba04A23F0d9D9F2e3B7C05F4948C99b37")
+    console.log("balance ",balance)
+    console.log("balance on this address is",ethers.formatEther(balance))
+
+    const totalSupply=await contract._tTotal()
+
+    // convert wei to eth
+    const totalSupplyInEther=ethers.formatEther(totalSupply)
+    
+    console.log(totalSupply)
+    console.log(totalSupplyInEther)
+
+
+
+}
+interactionSc()
